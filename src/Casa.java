229a1a97a731f1ca69183db0d01f60c088b5894a
Fabiano Dutra <lfdjesus@gmail.com>
2023-06-09 14:39:34 -0300

    Commit utilizando a IDE

--- src/Casa.java
+++ src/Casa.java
@@ -10,7 +10,7 @@ public class Casa extends Imovel {
 	}
 	public void imprimeDadosCasa() {
 		System.out.println("*****Tipo de Imovel CASA******");
-		System.out.println("Rua Frei Estanislau Shaethe, 61 - Vila Nova - Blumenau/SC");
+		System.out.println("Rua Pedro Krauss, 99 - Wostard - Blumenau/SC");
 		System.out.println("Preco Total do Imovel R$ "+ (getPreco()+getAdicional()));
 		
 	}
